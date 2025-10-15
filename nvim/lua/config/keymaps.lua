d88679b50867b4c2fbf26b343382572fb4384682
Peraso Lab <lab@perasotech.com>
2025-10-15 14:55:14 -0400

    tmux and nvim config fixes

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,12 +1,23 @@
 local map = vim.keymap.set
 
 map("n", "<leader>ln", function()
-  if vim.wo.number then
-    vim.wo.number = false
+  local wo = vim.wo
+  if wo.number or wo.relativenumber then
+      wo.relativenumber = false
+      wo.number = false
   else
-    vim.wo.number = true
-    vim.wo.relativenumber = false
+    wo.number = true
+    wo.relativenumber = true
   end
-end, { desc = "Toggle absolute line numbers on/off" })
+end, { desc = "Toggle hybrid line numbers on/off" })
 
-map("n", "<leader>cf", ":%y+<CR>", { desc = "Copy entire file to system clipboard (+)" })
+map("v", "<C-c>", function()
+  vim.cmd('normal! "zy')                 -- yank selection -> "z
+  local text = vim.fn.getreg("z")
+  if not text or #text == 0 then return end
+  local job = vim.fn.jobstart({ "tmux", "load-buffer", "-w", "-" }, { stdin = "pipe" })
+  if job > 0 then
+    vim.fn.chansend(job, text)
+    vim.fn.chanclose(job, "stdin")
+  end
+end, { desc = "Copy via tmux buffer" })
--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -1,6 +1,6 @@
-vim.opt.mouse = ""
+vim.opt.mouse = "a"
 vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 vim.opt.number = true
-vim.opt.relativenumber = false
+vim.opt.relativenumber = true
