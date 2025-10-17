caba915c1f46ca8b67d61ab3fb37c95ae2772045
Peraso Lab <lab@perasotech.com>
2025-10-17 12:02:47 -0400

    Visual selection live grep

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -11,6 +11,26 @@ return {
     vim.keymap.set("n", "<leader>fg", function()
       builtin.live_grep()
     end, { desc = "Telescope: Live grep", silent = true })
+  
+    local function get_visual_selection()
+      local bufnr = 0
+      local srow, scol = unpack(vim.api.nvim_buf_get_mark(bufnr, "<"))
+      local erow, ecol = unpack(vim.api.nvim_buf_get_mark(bufnr, ">"))
+      if srow == 0 or erow == 0 then return "" end
+      local lines = vim.api.nvim_buf_get_text(bufnr, srow - 1, scol, erow - 1, ecol + 1, {})
+      local text = table.concat(lines, "\n"):gsub("^%s+", ""):gsub("%s+$", ""):gsub("\n+", " ")
+      return text
+    end
+
+    vim.keymap.set("v", "<leader>fg", function()
+      local text = get_visual_selection()
+      if text ~= "" then
+        builtin.live_grep({
+          default_text = text,
+          additional_args = function() return { "--fixed-strings" } end,
+        })
+      end
+    end, { desc = "Telescope: Live grep selection", silent = true })
   end,
 }
 
